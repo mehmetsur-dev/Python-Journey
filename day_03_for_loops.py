f089e3d222f27067dfb08b0f68c7b9aa4bba3391
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-05-07 07:57:35 +0300

    feat: add scripts from week 1 (Day 1-6)

---
+++ day_03_for_loops.py
@@ -0,0 +1,13 @@
+# (for) and (in)
+
+ages = [15, 25, 18, 12, 30, 20]
+
+
+# this means grab one age form ages list
+for age in ages:
+    #this means check that age 
+    if age >= 18:
+        print("u can join")
+
+    else:
+        print("u cant pass")
