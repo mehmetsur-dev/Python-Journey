4c3d32fff81abf473dbb1606ab3fbeaf5aa66aa3
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-05-21 08:08:39 +0300

    Add files via upload

---
+++ tutorials/Tutorial_6_challenge_1.py
@@ -0,0 +1,14 @@
+# Tutorial 6 - challenge
+# mehmet - day 16
+
+user = "Admin"
+logged_in = True
+
+if user == "Admin" and logged_in:
+    print("Welcome Admin")
+
+elif user != "Admin" and logged_in:
+    print("welcome")
+
+else:
+    print("Please log in")
