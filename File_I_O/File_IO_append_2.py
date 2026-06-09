b7bb4872707e3e7e457052d4156b930dbd4351fd
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-06-09 09:29:33 +0300

    Remove duplicate folders - keep properly named ones

--- File_I_O/File_IO_append_2.py
+++
@@ -1,8 +0,0 @@
-# Challenge 2 - Activity Log
-# Day 32 - File I/O Append Mode
-
-with open("log.txt", "a") as af:
-    af.write("[Run 3] User logged in\n")
-
-with open("log.txt", "r") as rf:
-    print(rf.read())
