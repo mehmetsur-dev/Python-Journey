168b8d1b7a152d93318ea0135b39a20505c311c7
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-07-01 10:41:33 +0300

    Day 56: Templates Part 1 - inheritance, context, and control tags
    - Set up templates/blog/ folder structure for app-level templates
    - Added home and about views using render() with context dicts
    - Built base.html with block content and if/else conditional title
    - Implemented template inheritance via extends/block/endblock in home.html and about.html
    - Used for loop template tag to render posts dynamically
    - Fixed block/endblock name mismatch (comtent -> content) typo bug

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Mehmet Sur',
+        'title': 'Blog poste',
+        'content': 'First poste content',
+        'date_posted': 'Aug 27, 2025'
+    },
+    {
+        'author': 'SeraCl',
+        'title': 'Blog poste 2',
+        'content': 'Second poste content',
+        'date_posted': 'Aug 28, 2025'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context,)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
