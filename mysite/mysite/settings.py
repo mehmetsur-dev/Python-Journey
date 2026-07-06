022f558a22225c9fdf7ad91d2ed83c072b1ccd36
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-07-06 12:07:11 +0300

    Complete Django Part 6: User Registration with Crispy Forms (fix Meta casing bug, add crispy-bootstrap5)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
