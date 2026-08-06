8e0c3ae8148855ea2fcb54673d37084766bcce0f
Mehmet Sur <mehmetsur.dev@gmail.com>
2026-08-06 10:46:29 +0300

    Day 72: Switch EMAIL_BACKEND to console backend for local dev
    - Set EMAIL_BACKEND to django.core.mail.backends.console.EmailBackend
    - Comment out SMTP config (EMAIL_HOST, EMAIL_PORT, EMAIL_USE_TLS,
      EMAIL_HOST_USER, EMAIL_HOST_PASSWORD) for local testing without
      real credentials

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for mysite project.
 
@@ -128,3 +130,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
